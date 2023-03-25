4e40d5855197916e5f162f8549cc63617c778a83
Swati-Singh-135 <singh135swati@gmail.com>
2023-03-26 00:43:45 +0530

    Finished

---
+++ ImageOperation.java
@@ -0,0 +1,86 @@
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JTextField;
+import javax.swing.plaf.FileChooserUI;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class ImageOperation implements ActionListener
+{
+    public static void operate(int key)
+    {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.showOpenDialog(null);
+        File file = fileChooser.getSelectedFile();
+
+        //! FileInputStream
+        try{
+                FileInputStream fis=new FileInputStream(file);
+
+                byte []data=new byte[fis.available()];
+                fis.read(data);
+                int i=0;
+                for(byte b:data)
+                {
+                    data[i]=(byte)(b^key);
+                    i++;
+                }
+                FileOutputStream fos=new FileOutputStream(file);
+                fos.write(data);
+                fos.close();
+                fis.close();
+                JOptionPane.showMessageDialog(null,"Done");
+
+        }catch(Exception e)
+        {
+
+        }
+    }
+
+    public static void main (String args[])
+    {
+        //? creating frame
+        JFrame fr = new JFrame();
+        fr.setTitle("Image Operation");
+        fr.setSize(400, 400);
+        fr.setLocationRelativeTo(null);
+        fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //? Font
+        Font font = new Font("Roboto", Font.BOLD, 25);
+
+        
+        //! creating button
+        JButton btn = new JButton();
+        btn.setText("Open Image");
+        btn.setFont(font);
+
+
+        //! creating textField
+        JTextField textField = new JTextField(10);
+        textField.setFont(font);
+
+        //? using lamda function
+        btn.addActionListener(e->{
+            String text = textField.getText();
+            int temp = Integer.parseInt(text);
+
+            operate(temp);
+        });
+
+
+        fr.setLayout(new FlowLayout());
+        fr.add(btn);
+        fr.add(textField);
+        fr.setVisible(true);
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+- Encrypt and decrypt images using a numerical pin code to protect them from unauthorized access.
+- Utilize cryptographic algorithms to scramble and unscramble the image data during the encryption and decryption process.
+- Secure images by requiring users to enter a pin code to access them, preventing unauthorized individuals from viewing sensitive information.
+- Enhance personal privacy, business security, legal documentation, military intelligence, and medical records by using your project to encrypt and decrypt images.
+- **Tech Stack Used** : Java
